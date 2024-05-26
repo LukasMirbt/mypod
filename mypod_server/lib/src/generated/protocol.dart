e3a6613e826f75eb7bed354e332832f43da779e6
LukasMirbt <lukas.mirbt@appshack.se>
2024-05-26 18:58:47 +0200

    fix stuff

--- mypod_server/lib/src/generated/protocol.dart
+++ mypod_server/lib/src/generated/protocol.dart
@@ -11,8 +11,7 @@ library protocol; // ignore_for_file: no_leading_underscores_for_library_prefixe
 
 import 'package:serverpod/serverpod.dart' as _i1;
 import 'package:serverpod/protocol.dart' as _i2;
-import 'package:serverpod_auth_server/module.dart' as _i3;
-import 'example.dart' as _i4;
+import 'example.dart' as _i3;
 export 'example.dart';
 
 class Protocol extends _i1.SerializationManagerServer {
@@ -69,7 +68,6 @@ class Protocol extends _i1.SerializationManagerServer {
       ],
       managed: true,
     ),
-    ..._i3.Protocol.targetTableDefinitions,
     ..._i2.Protocol.targetTableDefinitions,
   ];
 
@@ -82,15 +80,12 @@ class Protocol extends _i1.SerializationManagerServer {
     if (customConstructors.containsKey(t)) {
       return customConstructors[t]!(data, this) as T;
     }
-    if (t == _i4.Example) {
-      return _i4.Example.fromJson(data, this) as T;
+    if (t == _i3.Example) {
+      return _i3.Example.fromJson(data, this) as T;
     }
-    if (t == _i1.getType<_i4.Example?>()) {
-      return (data != null ? _i4.Example.fromJson(data, this) : null) as T;
+    if (t == _i1.getType<_i3.Example?>()) {
+      return (data != null ? _i3.Example.fromJson(data, this) : null) as T;
     }
-    try {
-      return _i3.Protocol().deserialize<T>(data, t);
-    } catch (_) {}
     try {
       return _i2.Protocol().deserialize<T>(data, t);
     } catch (_) {}
@@ -99,12 +94,7 @@ class Protocol extends _i1.SerializationManagerServer {
 
   @override
   String? getClassNameForObject(Object data) {
-    String? className;
-    className = _i3.Protocol().getClassNameForObject(data);
-    if (className != null) {
-      return 'serverpod_auth.$className';
-    }
-    if (data is _i4.Example) {
+    if (data is _i3.Example) {
       return 'Example';
     }
     return super.getClassNameForObject(data);
@@ -112,12 +102,8 @@ class Protocol extends _i1.SerializationManagerServer {
 
   @override
   dynamic deserializeByClassName(Map<String, dynamic> data) {
-    if (data['className'].startsWith('serverpod_auth.')) {
-      data['className'] = data['className'].substring(15);
-      return _i3.Protocol().deserializeByClassName(data);
-    }
     if (data['className'] == 'Example') {
-      return deserialize<_i4.Example>(data['data']);
+      return deserialize<_i3.Example>(data['data']);
     }
     return super.deserializeByClassName(data);
   }
@@ -125,20 +111,14 @@ class Protocol extends _i1.SerializationManagerServer {
   @override
   _i1.Table? getTableForType(Type t) {
     {
-      var table = _i3.Protocol().getTableForType(t);
-      if (table != null) {
-        return table;
-      }
-    }
-    {
       var table = _i2.Protocol().getTableForType(t);
       if (table != null) {
         return table;
       }
     }
     switch (t) {
-      case _i4.Example:
-        return _i4.Example.t;
+      case _i3.Example:
+        return _i3.Example.t;
     }
     return null;
   }
